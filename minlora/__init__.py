52bb82c9c9bc93cc3c9aee091ac203c1f68a80ea
abhisek <a.maiti@utwente.nl>
2023-05-01 13:25:59 +0200

    fix init

--- minlora/__init__.py
+++ minlora/__init__.py
@@ -1,4 +1,6 @@
-from minlora.model import LoRAParametrization, add_lora, default_lora_config, merge_lora, remove_lora
+from minlora.model import (
+    LoRAParametrization, add_lora, default_lora_config, merge_lora, remove_lora
+)
 from minlora.utils import (
     apply_to_lora,
     disable_lora,
@@ -6,9 +8,10 @@ from minlora.utils import (
     get_parameters,
     get_lora_state_dict,
     load_multiple_lora,
-    name_is_lora,
-    name_is_bias,
+    if_lora,
+    if_bias,
     select_lora,
     tie_weights,
     untie_weights,
+    parameter_groups
 )
